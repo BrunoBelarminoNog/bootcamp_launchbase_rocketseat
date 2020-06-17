0c1c42243bf17775e54da99f4794633342055497
Bruno Belarmino <brunobelarm.nog@gmail.com>
2020-06-17 19:59:25 -0300

    js

---
+++ works/module_02/scripts.js
@@ -0,0 +1,15 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+const closeModal = document.querySelector('.close-modal');
+
+for (let card of cards){
+    card.addEventListener('click', function(){
+        modalOverlay.classList.add('active')
+    })
+}
+
+closeModal.addEventListener('click', function () {
+    modalOverlay.classList.remove('active')
+})
+
+modalOverlay.querySelector('iframe').src = 'https://www.youtube.com/embed/XcU9GEUZTQA'
