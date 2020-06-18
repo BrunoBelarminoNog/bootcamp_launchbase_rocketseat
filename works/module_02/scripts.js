e4be0bb2b8036390d78f9654607459e1fd875f68
Bruno Belarmino <brunobelarm.nog@gmail.com>
2020-06-17 22:00:15 -0300

    challange 02

--- works/module_02/scripts.js
+++ works/module_02/scripts.js
@@ -4,12 +4,14 @@ const closeModal = document.querySelector('.close-modal');
 
 for (let card of cards){
     card.addEventListener('click', function(){
+        const videoId = card.getAttribute('id')
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoId}`
+
     })
 }
 
 closeModal.addEventListener('click', function () {
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ""
 })
-
-modalOverlay.querySelector('iframe').src = 'https://www.youtube.com/embed/XcU9GEUZTQA'
